6174b0c1c83bfbf95c0e4d8bb54d87647a70dd7c
AmberlinMiller <amberlincl@gmail.com>
2022-06-01 15:23:30 +0800

    add gsap

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ console.log(aa(9));
 
 // jquery
 import $ from 'jquery';
+
+import { gsap } from "gsap";
+
+gsap.to('.logo' ,{
+   duration:1 ,
+x: 300,
+y :10,
+repeat: -1,
+yoyo : true,
+rotation:360
+});
+
 import './css/style.css';
 import './css/header.css';
 import './css/footer.css';
