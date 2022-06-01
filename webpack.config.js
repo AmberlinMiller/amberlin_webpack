a6def01fa7b9db47168d16cf291e488838c5bab1
AmberlinMiller <amberlincl@gmail.com>
2022-06-01 14:14:40 +0800

    update .css in one css doc

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',               // 入口文件
@@ -6,17 +7,28 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },              // 出口文件
-    module: {rules: [{ // 處裡對應模組
-        // 格式
-        test: /\.css$/,
-        //順序是由下到上 css > style
-        use: [
-            'style-loader',
-            'css-loader'
+    module: {
+        rules: [{
+            // 格式
+            test: /\.css$/,
+            //順序是由下到上 css > style
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: './dist'
+                }
+              },
+                //'style-loader', 會跟原本的衝突 
+                'css-loader'
             ],
         }]
+
     },              
-    // plugins: [],             // 對應的插件
+    plugins: [ // 對應的插件
+        new MiniCssExtractPlugin({
+            filename: "./css/style.css"
+        })
+    ],             
     // devServer: {},           // 服務器配置
     mode: 'development'      // 開發模式配置 // 上線用 production
 }
